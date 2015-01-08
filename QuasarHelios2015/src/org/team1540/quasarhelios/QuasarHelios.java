d1ffb3fc8028d4de689fb8b89d253435f698a7c7
Colby Skeggs <skeggsc@catlin.edu>
2015-01-07 17:11:48 -0800

    Add skeleton code for QuasarHelios

---
+++ QuasarHelios2015/src/org/team1540/quasarhelios/QuasarHelios.java
@@ -0,0 +1,14 @@
+package org.team1540.quasarhelios;
+
+import ccre.igneous.Igneous;
+import ccre.igneous.IgneousApplication;
+
+/**
+ * The main class for QuasarHelios. This dispatches to all of the other modules.
+ */
+public class QuasarHelios implements IgneousApplication {
+
+    public void setupRobot() {
+        // Robot setup code goes here.
+    }
+}
