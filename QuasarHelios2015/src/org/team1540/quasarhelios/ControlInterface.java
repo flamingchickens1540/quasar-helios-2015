b3e3eddcea26d5d7bb217365e438985fa5812f17
Alexander Mackworth <alexander@tssr.co>
2015-01-08 16:04:56 -0800

    Basic ControlInterface skeleton.

--- QuasarHelios2015/src/org/team1540/quasarhelios/ControlInterface.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/ControlInterface.java
@@ -1,5 +1,25 @@
 package org.team1540.quasarhelios;
 
+import ccre.igneous.Igneous;
+import ccre.holders.TuningContext;
+
 public class ControlInterface {
+	public static TuningContext autoTuning = new TuningContext("Autonomous").publishSavingEvent();
+	public static TuningContext teleTuning = new TuningContext("Teleoperated").publishSavingEvent();
 	
+	public static void setup() {
+		setupJoysticks();
+		setupCluck();
+	}
+	
+	public static void setupJoysticks() {
+		DriveCode.leftJoystickChannel = Igneous.joystick1.getYAxisSource();
+		DriveCode.rightJoystickChannel = Igneous.joystick2.getYAxisSource();		
+	}
+	public static void setupPhidget() {
+	
+	}
+	public static void setupCluck() {
+		
+	}
 }
