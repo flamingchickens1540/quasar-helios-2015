26c9483c5cfbeb7df8a56eb00aeed1d157c45ae8
Alexander Mackworth <alexander@tssr.co>
2015-01-08 16:04:56 -0800

    Basic ControlInterface skeleton.

--- QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
@@ -1,6 +1,11 @@
 package org.team1540.quasarhelios;
 
+import ccre.channel.FloatInput;
+
 public class DriveCode {
+	public static FloatInput leftJoystickChannel;
+	public static FloatInput rightJoystickChannel;
+	
 	public static void setup() {
 		
 	}
