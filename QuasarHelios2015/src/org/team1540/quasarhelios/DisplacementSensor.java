38d70410a0f5f6bdb81d98d8cb16fc9a86c86391
Vincent Miller <millerv@catlin.edu>
2015-01-09 16:02:42 -0800

    Added classes for DisplacementSensor and HeadingSensor

---
+++ QuasarHelios2015/src/org/team1540/quasarhelios/DisplacementSensor.java
@@ -0,0 +1,13 @@
+package org.team1540.quasarhelios;
+
+import java.awt.geom.Point2D;
+
+public class DisplacementSensor {
+	public static void setup() {
+		
+	}
+	
+	public static Point2D getDisplacement() {
+		throw new UnsupportedOperationException("Not Implemented Yet");
+	}
+}
---
+++ QuasarHelios2015/src/org/team1540/quasarhelios/HeadingSensor.java
@@ -0,0 +1,11 @@
+package org.team1540.quasarhelios;
+
+public class HeadingSensor {
+	public static void setup() {
+		
+	}
+	
+	public static float getHeading() {
+		throw new UnsupportedOperationException("Not Implemented Yet");
+	}
+}
