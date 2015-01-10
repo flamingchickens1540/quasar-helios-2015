2d733ab751582e26bb53c36012c68d1aaa75632b
Vincent Miller <millerv@catlin.edu>
2015-01-09 16:55:22 -0800

    Removed AWT dependencies

--- QuasarHelios2015/src/org/team1540/quasarhelios/DisplacementSensor.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/DisplacementSensor.java
@@ -1,13 +1,11 @@
 package org.team1540.quasarhelios;
 
-import java.awt.geom.Point2D;
-
 public class DisplacementSensor {
 	public static void setup() {
 		
 	}
 	
-	public static Point2D getDisplacement() {
+	public static Vector2 getDisplacement() {
 		throw new UnsupportedOperationException("Not Implemented Yet");
 	}
 }
