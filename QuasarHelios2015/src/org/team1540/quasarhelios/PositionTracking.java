8ee2552c97294b53dbaed0cd895ea742867af729
Vincent Miller <millerv@catlin.edu>
2015-01-09 16:58:10 -0800

    Implemented basic integration in PositionTracking

--- QuasarHelios2015/src/org/team1540/quasarhelios/PositionTracking.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/PositionTracking.java
@@ -1,7 +1,34 @@
 package org.team1540.quasarhelios;
 
 public class PositionTracking {
+	private static Vector2 position;
+	private static boolean running;
+	
 	public static void setup() {
+		DisplacementSensor.setup();
+		HeadingSensor.setup();
 		
+		position = new Vector2(0.0f, 0.0f);
+		running = true;
+		
+		new Thread() {
+			@Override
+			public void run() {
+				while (running) {
+					float heading = HeadingSensor.getHeading();
+					Vector2 displacement = DisplacementSensor.getDisplacement();
+					
+					position = position.plus(displacement.rotate((float)Math.toRadians(heading)));
+				}
+			}
+		}.start();
+	}
+	
+	public static void destroy() {
+		running = false;
+	}
+	
+	public static Vector2 getPosition() {
+		return position;
 	}
 }
