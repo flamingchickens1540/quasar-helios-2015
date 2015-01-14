40ff4dc1dd0fede9c0071ceb5f02bec5a9535a5d
Aidan Smith <smithai@catlin.edu>
2015-01-13 23:17:59 -0800

    Testing: Added Motors

--- QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
@@ -1,12 +1,17 @@
 package org.team1540.quasarhelios;
 
-import ccre.channel.BooleanInput;
-import ccre.channel.FloatInput;
+import ccre.channel.*;
+import ccre.igneous.*;
 
 public class DriveCode {
 	public static FloatInput leftJoystickChannel;
 	public static FloatInput rightJoystickChannel;
 	public static BooleanInput octocanumShifting;
+	private static FloatOutput rightFrontMotor = Igneous.makeTalonMotor(1, Igneous.MOTOR_FORWARD, .1f);
+	private static FloatOutput leftFrontMotor = Igneous.makeTalonMotor(2, Igneous.MOTOR_FORWARD, .1f);
+	private static FloatOutput rightBackMotor = Igneous.makeTalonMotor(3, Igneous.MOTOR_FORWARD, .1f);
+	private static FloatOutput leftBackMotor = Igneous.makeTalonMotor(4, Igneous.MOTOR_FORWARD, .1f);
+
 	
 	public static void setup() {
 		
