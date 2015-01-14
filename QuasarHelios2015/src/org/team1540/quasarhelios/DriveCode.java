b065ac2f42285dcf6fdc8673b8db8623b20ccb53
AidanSmith <smithai@catlin.edu>
2015-01-14 12:27:48 -0800

    Reversed left motors

--- QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
@@ -9,13 +9,15 @@ public class DriveCode {
 	public static FloatInput rightJoystickChannel;
 	public static BooleanInput octocanumShifting;
 	private static FloatOutput rightFrontMotor = Igneous.makeTalonMotor(1, Igneous.MOTOR_FORWARD, .1f);
-	private static FloatOutput leftFrontMotor = Igneous.makeTalonMotor(2, Igneous.MOTOR_FORWARD, .1f);
+	private static FloatOutput leftFrontMotor = Igneous.makeTalonMotor(2, Igneous.MOTOR_REVERSE, .1f);
 	private static FloatOutput rightBackMotor = Igneous.makeTalonMotor(3, Igneous.MOTOR_FORWARD, .1f);
-	private static FloatOutput leftBackMotor = Igneous.makeTalonMotor(4, Igneous.MOTOR_FORWARD, .1f);
+	private static FloatOutput leftBackMotor = Igneous.makeTalonMotor(4, Igneous.MOTOR_REVERSE, .1f);
 	private static FloatOutput rightMotors = FloatMixing.combine(rightFrontMotor, rightBackMotor);
 	private static FloatOutput leftMotors = FloatMixing.combine(leftFrontMotor, leftBackMotor);
 	
 	public static void setup() {
 		
 	}
+	
+	
 }
