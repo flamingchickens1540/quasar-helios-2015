f8961233a7a80a08455903ee5d52111183f683e4
Alexander Mackworth <alexander@tssr.co>
2015-01-13 16:52:04 -0800

    Implement Elevator.

--- QuasarHelios2015/src/org/team1540/quasarhelios/Elevator.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Elevator.java
@@ -1,5 +1,21 @@
 package org.team1540.quasarhelios;
 
-public class Elevator {
+import ccre.channel.BooleanOutput;
+import ccre.channel.BooleanStatus;
+import ccre.channel.FloatOutput;
+import ccre.ctrl.Mixing;
+import ccre.igneous.Igneous;
 
+public class Elevator {
+	private static final FloatOutput winch = Igneous.makeJaguarMotor(5, Igneous.MOTOR_FORWARD, 0.4f);
+	
+	private static final BooleanStatus raisingStatus = new BooleanStatus(Mixing.select(winch, 0.0f, 1.0f));
+	private static final BooleanStatus loweringStatus = new BooleanStatus(Mixing.select(winch, 0.0f, -1.0f));
+	
+	public static final BooleanOutput elevatorControl = new BooleanOutput() {
+		public void set(boolean value) {
+			raisingStatus.set(value);
+			loweringStatus.set(!value);
+		}
+	};
 }
