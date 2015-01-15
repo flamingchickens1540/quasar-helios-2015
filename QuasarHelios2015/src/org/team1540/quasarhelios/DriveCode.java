385f61f52804a12dcd8355f57a302573069243f4
AidanSmith <smithai@catlin.edu>
2015-01-14 23:37:08 -0800

    Basic mecanum to test

--- QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
@@ -1,12 +1,14 @@
 package org.team1540.quasarhelios;
 
 import ccre.channel.*;
-import ccre.ctrl.FloatMixing;
+import ccre.ctrl.*;
 import ccre.igneous.*;
 
 public class DriveCode {
-	public static FloatInput leftJoystickChannel;
-	public static FloatInput rightJoystickChannel;
+	public static FloatInput leftJoystickChannelX;
+	public static FloatInput leftJoystickChannelY;
+	public static FloatInput rightJoystickChannelX;
+	public static FloatInput rightJoystickChannelY;
 	public static BooleanInput octocanumShifting;
 	private static FloatOutput rightFrontMotor = Igneous.makeTalonMotor(1, Igneous.MOTOR_FORWARD, .1f);
 	private static FloatOutput leftFrontMotor = Igneous.makeTalonMotor(2, Igneous.MOTOR_REVERSE, .1f);
@@ -15,9 +17,50 @@ public class DriveCode {
 	private static FloatOutput rightMotors = FloatMixing.combine(rightFrontMotor, rightBackMotor);
 	private static FloatOutput leftMotors = FloatMixing.combine(leftFrontMotor, leftBackMotor);
 	
-	public static void setup() {
-		
-	}
+	private static double π = Math.PI;
 	
+	public static void setup() {
+		Igneous.duringTele.send(new EventOutput(){
+			public void event() {
+				float distanceY = rightJoystickChannelY.get();
+				float distanceX = rightJoystickChannelX.get();
+				float speed = leftJoystickChannelY.get();
+				float rotationspeed = leftJoystickChannelX.get();
+				double angle;
+				if (distanceX==0){
+					if (distanceY>=0){
+						angle = π/2;
+					}
+					else{
+						angle = 3*π/2;
+					}
+				}
+				else{
+					angle = Math.atan(distanceY/distanceX);
+					if (distanceX<0){
+						angle+=π;
+					}
+					if (angle<0){
+						angle+=2*π;
+					}
+				}
+				float leftFront = (float) (speed*Math.sin(angle+π/4)+rotationspeed);
+				float rightFront = (float) (speed*Math.cos(angle+π/4)-rotationspeed);
+				float leftBack = (float) (speed*Math.cos(angle+π/4)+rotationspeed);
+				float rightBack = (float) (speed*Math.sin(angle+π/4)-rotationspeed);
+				float normalize = Math.max(Math.max(Math.abs(leftFront), Math.abs(rightFront)), Math.max(Math.abs(leftBack), Math.abs(rightBack)));
+				if (normalize>1){
+					leftFront = leftFront/normalize;
+					rightFront = rightFront/normalize;
+					leftBack = leftBack/normalize;
+					rightBack = rightBack/normalize;
+				}
+				rightFrontMotor.set(rightFront);
+				leftFrontMotor.set(leftFront);
+				rightBackMotor.set(rightBack);
+				leftBackMotor.set(leftBack);
+			}
+		});
+	}
 	
 }
