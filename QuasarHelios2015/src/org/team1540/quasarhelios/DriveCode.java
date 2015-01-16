f32152fe30109452094843a02f01fee499d69bdb
AidanSmith <smithai@catlin.edu>
2015-01-15 17:59:51 -0800

    Reorganized code

--- QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/DriveCode.java
@@ -16,55 +16,55 @@ public class DriveCode {
 	private static FloatOutput rightBackMotor = Igneous.makeTalonMotor(4, Igneous.MOTOR_FORWARD, .1f);
 	private static FloatOutput rightMotors = FloatMixing.combine(rightFrontMotor, rightBackMotor);
 	private static FloatOutput leftMotors = FloatMixing.combine(leftFrontMotor, leftBackMotor);
-
+	
 	private static double π = Math.PI;
+	
+	private static EventOutput mecanum = new EventOutput() {
+		public void event() {
+			float distanceY = rightJoystickChannelY.get();
+			float distanceX = rightJoystickChannelX.get();
+			float speed = leftJoystickChannelY.get();
+			float rotationspeed = leftJoystickChannelX.get();
+			double angle;
+			if (distanceX == 0) {
+				if (distanceY >= 0) {
+					angle = π / 2;
+				} else {
+					angle = 3 * π / 2;
+				}
+			} else {
+				angle = Math.atan(distanceY / distanceX);
+				if (distanceX < 0) {
+					angle += π;
+				}
+			}
+			float leftFront = (float) (speed * Math.sin(angle - π / 4) + rotationspeed);
+			float rightFront = (float) (speed * Math.cos(angle - π / 4) - rotationspeed);
+			float leftBack = (float) (speed * Math.cos(angle - π / 4) + rotationspeed);
+			float rightBack = (float) (speed * Math.sin(angle - π / 4) - rotationspeed);
+			float normalize = Math.max(Math.max(Math.abs(leftFront), Math.abs(rightFront)), Math.max(Math.abs(leftBack), Math.abs(rightBack)));
+			if (normalize > 1) {
+				leftFront /= normalize;
+				rightFront /= normalize;
+				leftBack /= normalize;
+				rightBack /= normalize;
+			}
+			if (normalize < Math.abs(speed)) {
+				float multiplier = Math.abs(speed) / normalize;
+				leftFront *= multiplier;
+				rightFront *= multiplier;
+				leftBack *= multiplier;
+				rightBack *= multiplier;
+			}
+			rightFrontMotor.set(rightFront);
+			leftFrontMotor.set(leftFront);
+			rightBackMotor.set(rightBack);
+			leftBackMotor.set(leftBack);
+		}
+	};
 
 	public static void setup() {
-		Igneous.duringTele.send(new EventOutput() {
-			public void event() {
-				float distanceY = rightJoystickChannelY.get();
-				float distanceX = rightJoystickChannelX.get();
-				float speed = leftJoystickChannelY.get();
-				float rotationspeed = leftJoystickChannelX.get();
-				double angle;
-				if (distanceX == 0) {
-					if (distanceY >= 0) {
-						angle = π / 2;
-					} else {
-						angle = 3 * π / 2;
-					}
-				} else {
-					angle = Math.atan(distanceY / distanceX);
-					if (distanceX < 0) {
-						angle += π;
-					}
-				}
-				float leftFront = (float) (speed * Math.sin(angle - π / 4) + rotationspeed);
-				float rightFront = (float) (speed * Math.cos(angle - π / 4) - rotationspeed);
-				float leftBack = (float) (speed * Math.cos(angle - π / 4) + rotationspeed);
-				float rightBack = (float) (speed * Math.sin(angle - π / 4) - rotationspeed);
-				float normalize = Math.max(
-						Math.max(Math.abs(leftFront), Math.abs(rightFront)),
-						Math.max(Math.abs(leftBack), Math.abs(rightBack)));
-				if (normalize > 1) {
-					leftFront /= normalize;
-					rightFront /= normalize;
-					leftBack /= normalize;
-					rightBack /= normalize;
-				}
-				if (normalize < Math.abs(speed)) {
-					float multiplier = Math.abs(speed)/normalize;
-					leftFront *= multiplier;
-					rightFront *= multiplier;
-					leftBack *= multiplier;
-					rightBack *= multiplier;
-				}
-				rightFrontMotor.set(rightFront);
-				leftFrontMotor.set(leftFront);
-				rightBackMotor.set(rightBack);
-				leftBackMotor.set(leftBack);
-			}
-		});
+		Igneous.duringTele.send(mecanum);
 	}
 
 }
