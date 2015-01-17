3e087bf56796ee417e77f4bd174423521bca928e
Vincent Miller <millerv@catlin.edu>
2015-01-16 16:58:32 -0800

    Fixed various issues with Clamp

--- QuasarHelios2015/src/org/team1540/quasarhelios/Clamp.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Clamp.java
@@ -3,9 +3,11 @@ package org.team1540.quasarhelios;
 import ccre.channel.BooleanInputPoll;
 import ccre.channel.BooleanOutput;
 import ccre.channel.EventInput;
+import ccre.channel.FloatInput;
 import ccre.channel.FloatInputPoll;
 import ccre.channel.FloatOutput;
 import ccre.channel.FloatStatus;
+import ccre.cluck.Cluck;
 import ccre.ctrl.BooleanMixing;
 import ccre.ctrl.EventMixing;
 import ccre.ctrl.FloatMixing;
@@ -13,37 +15,31 @@ import ccre.ctrl.PIDControl;
 import ccre.igneous.Igneous;
 
 public class Clamp {
-	public final BooleanOutput openControl;
+	public final BooleanOutput openControl = BooleanMixing.combine(Igneous.makeSolenoid(0), Igneous.makeSolenoid(1));
 	public final FloatOutput heightControl;
 	
-	private FloatStatus min;
-	private FloatStatus max;
+	private FloatStatus min = new FloatStatus(0.0f);
+	private FloatStatus max = new FloatStatus(1.0f);
 	
 	public Clamp() {
+		FloatStatus height = new FloatStatus();
+		heightControl = height;
 		
-		BooleanOutput leftClamp = Igneous.makeSolenoid(0);
-		BooleanOutput rightClamp = Igneous.makeSolenoid(1);
+		FloatInputPoll encoder = Igneous.makeEncoder(0, 1, false);
+		FloatOutput speedControl = Igneous.makeTalonMotor(2, Igneous.MOTOR_REVERSE, 0.1f);
 		
-		openControl = BooleanMixing.combine(leftClamp, rightClamp);
-		heightControl = new FloatStatus();
-		min = new FloatStatus(0.0f);
-		max = new FloatStatus(1.0f);
-		
-		FloatInputPoll encoder = Igneous.makeEncoder(0, 0, Igneous.MOTOR_FORWARD);
-		FloatOutput speedControl = Igneous.makeTalonMotor(2, Igneous.MOTOR_FORWARD, 1.0f);
-		
-		BooleanInputPoll inputLimitTop = Igneous.makeDigitalInput(0);
-		BooleanInputPoll inputLimitBottom = Igneous.makeDigitalInput(1);
-		
-		EventInput limitTop = EventMixing.filterEvent(inputLimitTop, true, Igneous.constantPeriodic);
-		EventInput limitBottom = EventMixing.filterEvent(inputLimitBottom, true, Igneous.constantPeriodic);
+		EventInput limitTop = EventMixing.filterEvent(Igneous.makeDigitalInput(2), true, Igneous.constantPeriodic);
+		EventInput limitBottom = EventMixing.filterEvent(Igneous.makeDigitalInput(3), true, Igneous.constantPeriodic);
 		
 		FloatMixing.pumpWhen(limitTop, encoder, max);
 		FloatMixing.pumpWhen(limitBottom, encoder, min);
 		
-		PIDControl pid = new PIDControl(FloatMixing.normalizeFloat(encoder, min, max), (FloatStatus)heightControl, FloatMixing.always(1.0f), FloatMixing.always(0.0f), FloatMixing.always(0.0f));
+		PIDControl pid = new PIDControl(FloatMixing.normalizeFloat(encoder, min, max), height, FloatMixing.always(1.0f), FloatMixing.always(0.0f), FloatMixing.always(0.0f));
+		
 		Igneous.constantPeriodic.send(pid);
 		
 		pid.send(speedControl);
+		
+		
 	}
 }
