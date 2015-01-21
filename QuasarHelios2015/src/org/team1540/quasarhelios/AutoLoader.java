8588da8e02f0fd01dd20b5a14a7c101b150b512d
Vincent Miller <millerv@catlin.edu>
2015-01-20 17:19:08 -0800

    Finished AutoLoader

--- QuasarHelios2015/src/org/team1540/quasarhelios/AutoLoader.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/AutoLoader.java
@@ -1,24 +1,49 @@
 package org.team1540.quasarhelios;
 
+import ccre.channel.BooleanInputPoll;
+import ccre.channel.BooleanStatus;
 import ccre.channel.EventOutput;
+import ccre.ctrl.BooleanMixing;
 import ccre.igneous.Igneous;
+import ccre.instinct.AutonomousModeOverException;
+import ccre.instinct.InstinctModule;
 
-public class AutoLoader implements EventOutput {
-	private Clamp clamp;
-	private BooleanInputPoll crateInPosition = Igneous.makeDigitalInput(0);
+public class AutoLoader extends InstinctModule {
+	private static BooleanInputPoll crateInPosition = Igneous.makeDigitalInput(0);
 	
-	public AutoLoader(Clamp clamp) {
-		this.clamp = clamp;
+	public BooleanStatus create() {
+		BooleanStatus b = new BooleanStatus(false);
+		AutoLoader a = new AutoLoader();
+		
+		a.setShouldBeRunning(b);
+		a.updateWhen(Igneous.globalPeriodic);
+		
+		BooleanMixing.whenBooleanBecomes(b, false).send(new EventOutput() {
+			@Override
+			public void event() {
+				Elevator.elevatorControl.set(false);
+			}
+		});
+		
+		return b;
 	}
 	
 	@Override
-	public void event() {
-		clamp.openControl.set(true);
-		clamp.heightControl.set(0.0f);
+	public void autonomousMain() throws AutonomousModeOverException, InterruptedException {
+		Elevator.elevatorControl.set(true);
 		
-		Rollers.allRollers.set(1.0f);
+		waitUntil(Elevator.topLimitSwitch);
 		
+		boolean r = Rollers.running.get();
 		
+		Rollers.direction.set(true);
+		Rollers.running.set(true);
+		
+		waitUntil(crateInPosition);
+		
+		Rollers.running.set(r);
+		
+		Elevator.elevatorControl.set(false);
 	}
 }
 
