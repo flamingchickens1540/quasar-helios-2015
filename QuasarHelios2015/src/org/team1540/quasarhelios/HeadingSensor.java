55f50f2f950b0ce32d23a66b19a408707239bf15
Vincent Miller <millerv@catlin.edu>
2015-01-21 16:15:28 -0800

    Added public fields in HeadingSensor for input channels

--- QuasarHelios2015/src/org/team1540/quasarhelios/HeadingSensor.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/HeadingSensor.java
@@ -1,13 +1,29 @@
 package org.team1540.quasarhelios;
 
+import ccre.channel.FloatInput;
 import ccre.drivers.chrobotics.UM7LT;
 import ccre.igneous.Igneous;
 
 public class HeadingSensor {
+	public static FloatInput pitch;
+	public static FloatInput yaw;
+	public static FloatInput roll;
+	
+	public static FloatInput pitchRate;
+	public static FloatInput yawRate;
+	public static FloatInput rollRate;
 
 	public static void setup(){
 		UM7LT sensor = new UM7LT(Igneous.makeRS232_MXP(115200, "UM7-LT"));
 		sensor.autoreportFaults.set(true);
 		sensor.start();
+		
+		pitch = sensor.pitch;
+		yaw = sensor.yaw;
+		roll = sensor.roll;
+		
+		pitchRate = sensor.pitchRate;
+		yawRate = sensor.yawRate;
+		rollRate = sensor.rollRate;
 	}
 }
