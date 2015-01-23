b3d78f7a15fe5aadb7ad9c2c297a0d4445c3d2c3
Vincent Miller <millerv@catlin.edu>
2015-01-22 17:17:31 -0800

    Changed name of "Compressor Enabled" to "Compressor Enable"

--- QuasarHelios2015/src/org/team1540/quasarhelios/Pressure.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Pressure.java
@@ -22,6 +22,6 @@ public class Pressure {
 		
 		Cluck.publish("Pressure Switch", pressureSwitch);
 		Cluck.publish("Pressure Gauge", pressureGauge);
-		Cluck.publish("Compressor Enabled", compressor);
+		Cluck.publish("Compressor Enable", compressor);
 	}
 }
