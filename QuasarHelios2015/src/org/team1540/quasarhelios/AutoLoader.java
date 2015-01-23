ab0eb4f3ff04a34e96c372ff591c59aacc80cbfd
Colby Skeggs <skeggsc@catlin.edu>
2015-01-22 17:11:43 -0800

    Fixed one last bug with the elevator during autoloading, and reformatted the file.

--- QuasarHelios2015/src/org/team1540/quasarhelios/AutoLoader.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/AutoLoader.java
@@ -8,43 +8,43 @@ import ccre.instinct.AutonomousModeOverException;
 import ccre.instinct.InstinctModule;
 
 public class AutoLoader extends InstinctModule {
-	private static final BooleanInputPoll crateInPosition = Igneous.makeDigitalInput(2);
-	
-	public static BooleanStatus create() {
-		BooleanStatus b = new BooleanStatus(false);
-		AutoLoader a = new AutoLoader();
-		
-		a.setShouldBeRunning(b);
-		a.updateWhen(Igneous.globalPeriodic);
-		
-		Elevator.raising.setFalseWhen(BooleanMixing.onRelease(b));
-		Elevator.lowering.setTrueWhen(BooleanMixing.onRelease(b));
-		
-		return b;
-	}
-	
-	@Override
-	public void autonomousMain() throws AutonomousModeOverException, InterruptedException {
-		Elevator.raising.set(true);
-		
-		waitUntil(Elevator.topLimitSwitch);
-		
-		boolean r = Rollers.running.get();
-		boolean d = Rollers.direction.get();
-		boolean o = Rollers.open.get();
-		
-		Rollers.direction.set(true);
-		Rollers.running.set(true);
-		Rollers.open.set(false);
-		
-		waitUntil(crateInPosition);
-		
-		Rollers.running.set(r);
-		Rollers.direction.set(d);
-		Rollers.open.set(o);
-		
-		Elevator.raising.set(false);
-		Elevator.lowering.set(true);
-	}
-}
+    private static final BooleanInputPoll crateInPosition = Igneous.makeDigitalInput(2);
 
+    public static BooleanStatus create() {
+        BooleanStatus b = new BooleanStatus(false);
+        AutoLoader a = new AutoLoader();
+
+        a.setShouldBeRunning(b);
+        a.updateWhen(Igneous.globalPeriodic);
+
+        Elevator.raising.setFalseWhen(BooleanMixing.onRelease(b));
+        Elevator.lowering.setTrueWhen(BooleanMixing.onRelease(b));
+
+        return b;
+    }
+
+    @Override
+    public void autonomousMain() throws AutonomousModeOverException, InterruptedException {
+        Elevator.lowering.set(false);
+        Elevator.raising.set(true);
+
+        waitUntil(Elevator.topLimitSwitch);
+
+        boolean r = Rollers.running.get();
+        boolean d = Rollers.direction.get();
+        boolean o = Rollers.open.get();
+
+        Rollers.direction.set(true);
+        Rollers.running.set(true);
+        Rollers.open.set(false);
+
+        waitUntil(crateInPosition);
+
+        Rollers.running.set(r);
+        Rollers.direction.set(d);
+        Rollers.open.set(o);
+
+        Elevator.raising.set(false);
+        Elevator.lowering.set(true);
+    }
+}
