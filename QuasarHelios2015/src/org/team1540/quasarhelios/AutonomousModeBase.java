3bb2176782ce2735b9d8905d5b2e310da192388c
Alexander Mackworth <alexander@tssr.co>
2015-01-22 18:18:47 -0800

    Fix formatting.

--- QuasarHelios2015/src/org/team1540/quasarhelios/AutonomousModeBase.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/AutonomousModeBase.java
@@ -10,16 +10,17 @@ import ccre.instinct.InstinctModeModule;
 public abstract class AutonomousModeBase extends InstinctModeModule {
 	public FloatInputPoll driveSpeed;
 	public FloatInputPoll rotateSpeed;
-	
+
 	public AutonomousModeBase(String modeName) {
 		super(modeName);
 	}
-	
-	protected void drive(float distance) throws AutonomousModeOverException, InterruptedException {
+
+	protected void drive(float distance) throws AutonomousModeOverException,
+			InterruptedException {
 		float startingEncoder = DriveCode.leftEncoder.get();
-		
+
 		if (distance > 0) {
-			DriveCode.allMotors.set(1.0f * driveSpeed.get());
+			DriveCode.allMotors.set(driveSpeed.get());
 			waitUntilAtLeast(DriveCode.leftEncoder, startingEncoder + distance);
 		} else {
 			DriveCode.allMotors.set(-1.0f * driveSpeed.get());
@@ -28,40 +29,45 @@ public abstract class AutonomousModeBase extends InstinctModeModule {
 
 		DriveCode.allMotors.set(0.0f);
 	}
-	
-	protected void turn(float degree) throws AutonomousModeOverException, InterruptedException {
+
+	protected void turn(float degree) throws AutonomousModeOverException,
+			InterruptedException {
 		float startingYaw = HeadingSensor.yaw.get();
 
 		if (degree > 0) {
-			DriveCode.rotate.set(1.0f * rotateSpeed.get());
+			DriveCode.rotate.set(rotateSpeed.get());
 			waitUntilAtLeast(HeadingSensor.yaw, startingYaw + degree);
 		} else {
 			DriveCode.rotate.set(-1.0f * rotateSpeed.get());
 			waitUntilAtMost(HeadingSensor.yaw, startingYaw + degree);
 		}
-		
+
 		DriveCode.rotate.set(0.0f);
 	}
-	
-	protected void collectTote() throws AutonomousModeOverException, InterruptedException {
+
+	protected void collectTote() throws AutonomousModeOverException,
+			InterruptedException {
 		QuasarHelios.autoLoader.set(true);
-		waitUntil(BooleanMixing.invert((BooleanInputPoll) QuasarHelios.autoLoader));
+		waitUntil(BooleanMixing
+				.invert((BooleanInputPoll) QuasarHelios.autoLoader));
 	}
-	
-    @Override
-    protected void autonomousMain() throws AutonomousModeOverException, InterruptedException {
-        try {
-            runAutonomous();
-        } finally {
-            DriveCode.allMotors.set(0);
-        }
-    }
-    
-    protected abstract void runAutonomous() throws InterruptedException, AutonomousModeOverException;
-    
-    public void loadSettings(TuningContext context) {
-    	this.driveSpeed = context.getFloat("auto-main-driveSpeed", 1.0f);
-    	this.rotateSpeed = context.getFloat("auto-main-rotateSpeed", 1.0f);
-    }
-    
+
+	@Override
+	protected void autonomousMain() throws AutonomousModeOverException,
+			InterruptedException {
+		try {
+			runAutonomous();
+		} finally {
+			DriveCode.allMotors.set(0);
+		}
+	}
+
+	protected abstract void runAutonomous() throws InterruptedException,
+			AutonomousModeOverException;
+
+	public void loadSettings(TuningContext context) {
+		this.driveSpeed = context.getFloat("auto-main-driveSpeed", 1.0f);
+		this.rotateSpeed = context.getFloat("auto-main-rotateSpeed", 1.0f);
+	}
+
 }
