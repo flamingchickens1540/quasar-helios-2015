17b1af73ae1e68570cf95f3874349d396709d954
Vincent Miller <millerv@catlin.edu>
2015-01-26 17:33:52 -0800

    Added auto-tuning for Pressure

--- QuasarHelios2015/src/org/team1540/quasarhelios/Pressure.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Pressure.java
@@ -2,7 +2,10 @@ package org.team1540.quasarhelios;
 
 import ccre.channel.BooleanInput;
 import ccre.channel.BooleanOutput;
+import ccre.channel.EventStatus;
 import ccre.channel.FloatInput;
+import ccre.channel.FloatInputPoll;
+import ccre.channel.FloatStatus;
 import ccre.cluck.Cluck;
 import ccre.ctrl.BooleanMixing;
 import ccre.ctrl.FloatMixing;
@@ -14,12 +17,23 @@ public class Pressure {
 	public static final BooleanOutput compressor = Igneous.usePCMCompressor();
 	
 	public static void setup() {
+		FloatInputPoll pressureInput = Igneous.makeAnalogInput(0);
 		
-		pressureGauge = FloatMixing.createDispatch(FloatMixing.normalizeFloat(Igneous.makeAnalogInput(0), 
-				ControlInterface.mainTuning.getFloat("main-pressure-min", 0.0f), ControlInterface.mainTuning.getFloat("pressure-max", 1.0f)), Igneous.globalPeriodic);
+		FloatStatus min = ControlInterface.mainTuning.getFloat("pressure-min", 0.0f);
+		FloatStatus max = ControlInterface.mainTuning.getFloat("pressure-max", 1.0f);
+		
+		EventStatus setMin = new EventStatus();
+		EventStatus setMax = new EventStatus();
+		
+		FloatMixing.pumpWhen(setMin, pressureInput, min);
+		FloatMixing.pumpWhen(setMax, pressureInput, max);
+		
+		pressureGauge = FloatMixing.createDispatch(FloatMixing.normalizeFloat(pressureInput, min, max), Igneous.globalPeriodic);
 		
 		Cluck.publish("Pressure Switch", pressureSwitch);
 		Cluck.publish("Pressure Gauge", pressureGauge);
 		Cluck.publish("Compressor Enable", compressor);
+		Cluck.publish("Pressure Min Set", setMin);
+		Cluck.publish("Pressure Max Set", setMax);
 	}
 }
