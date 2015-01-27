6b631e079d4c50a3df90bc617cb5c1eba2171603
Vincent Miller <millerv@catlin.edu>
2015-01-26 17:41:56 -0800

    Added auto-tuning for Clamp

--- QuasarHelios2015/src/org/team1540/quasarhelios/Clamp.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Clamp.java
@@ -4,6 +4,7 @@ import ccre.channel.BooleanInput;
 import ccre.channel.BooleanOutput;
 import ccre.channel.BooleanStatus;
 import ccre.channel.EventInput;
+import ccre.channel.EventStatus;
 import ccre.channel.FloatFilter;
 import ccre.channel.FloatInput;
 import ccre.channel.FloatInputPoll;
@@ -32,11 +33,14 @@ public class Clamp {
 		BooleanInput limitTop = BooleanMixing.createDispatch(Igneous.makeDigitalInput(2), Igneous.globalPeriodic);
 		BooleanInput limitBottom = BooleanMixing.createDispatch(Igneous.makeDigitalInput(3), Igneous.globalPeriodic);
 
-		FloatStatus min = ControlInterface.mainTuning.getFloat("main-clamp-min", 0.0f);
-		FloatStatus max = ControlInterface.mainTuning.getFloat("main-clamp-max", 1.0f);
+		FloatStatus min = ControlInterface.mainTuning.getFloat("clamp-min", 0.0f);
+		FloatStatus max = ControlInterface.mainTuning.getFloat("clamp-max", 1.0f);
+		
+		EventStatus setMin = new EventStatus();
+		EventStatus setMax = new EventStatus();
 
-		FloatMixing.pumpWhen(BooleanMixing.onPress(limitTop), encoder, max);
-		FloatMixing.pumpWhen(BooleanMixing.onPress(limitBottom), encoder, min);
+		FloatMixing.pumpWhen(EventMixing.combine(BooleanMixing.onPress(limitBottom), setMin), encoder, min);
+		FloatMixing.pumpWhen(EventMixing.combine(BooleanMixing.onPress(limitTop), setMax), encoder, max);
 
 		FloatStatus p = ControlInterface.mainTuning.getFloat("clamp-p", 1.0f);
 		FloatStatus i = ControlInterface.mainTuning.getFloat("clamp-i", 0.0f);
@@ -72,5 +76,7 @@ public class Clamp {
 		Cluck.publish(QuasarHelios.testPrefix + "Clamp Limit Top", limitTop);
 		Cluck.publish(QuasarHelios.testPrefix + "Clamp Limit Bottom", limitBottom);
 		Cluck.publish(QuasarHelios.testPrefix + "Clamp Motor Speed", speedControl);
+		Cluck.publish("Clamp Max Set", setMax);
+		Cluck.publish("Clamp Min Set", setMin);
 	}
 }
