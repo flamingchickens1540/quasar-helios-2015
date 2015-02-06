0096986fb7c6dc54a7ac701888347c3dd3bddc34
Vincent Miller <millerv@catlin.edu>
2015-02-06 15:52:12 -0800

    Fixed two issues with Elevator

--- QuasarHelios2015/src/org/team1540/quasarhelios/ControlInterface.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/ControlInterface.java
@@ -3,6 +3,7 @@ package org.team1540.quasarhelios;
 import ccre.igneous.Igneous;
 import ccre.channel.FloatInput;
 import ccre.cluck.Cluck;
+import ccre.ctrl.BooleanMixing;
 import ccre.ctrl.FloatMixing;
 import ccre.holders.TuningContext;
 
@@ -38,12 +39,14 @@ public class ControlInterface {
         Igneous.joystick2.getButtonSource(2).send(Elevator.setMiddle);
         Igneous.joystick2.getButtonSource(3).send(Elevator.setBottom);
         Igneous.joystick2.getButtonSource(4).send(Elevator.stop);
+        BooleanMixing.pumpWhen(QuasarHelios.globalControl, Igneous.joystick2.getButtonChannel(5), Elevator.overrideEnabled);
+        Igneous.joystick2.getAxisSource(1).send(Elevator.overrideValue);
 
-        Igneous.joystick2.getButtonSource(5).send(Rollers.runRollersButton);
-        Igneous.joystick2.getButtonSource(6).send(Rollers.toggleRollersButton);
-        Igneous.joystick2.getButtonSource(7).send(Rollers.toggleOpenButton);
-        Igneous.joystick2.getAxisSource(5).send(Rollers.controlArmsIndependently);
-        Igneous.joystick2.getAxisSource(6).send(Rollers.controlRollersManually);
+        Igneous.joystick2.getButtonSource(6).send(Rollers.runRollersButton);
+        Igneous.joystick2.getButtonSource(7).send(Rollers.toggleRollersButton);
+        Igneous.joystick2.getButtonSource(8).send(Rollers.toggleOpenButton);
+        Igneous.joystick2.getAxisSource(2).send(Rollers.controlArmsIndependently);
+        Igneous.joystick2.getAxisSource(3).send(Rollers.controlRollersManually);
 
         Clamp.heightInput = Igneous.joystick2.getAxisSource(1);
         Clamp.openInput = Igneous.joystick2.getButtonSource(8);
--- QuasarHelios2015/src/org/team1540/quasarhelios/Elevator.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Elevator.java
@@ -27,9 +27,9 @@ public class Elevator {
     private static final BooleanStatus raising = new BooleanStatus(false);
     private static final BooleanStatus lowering = new BooleanStatus(false);
     private static final BooleanStatus goingToMiddle = new BooleanStatus(false);
-    
-    private static final BooleanStatus overrideEnabled = new BooleanStatus(false);
-    private static final FloatStatus overrideValue = new FloatStatus(0.0f);
+
+    public static final BooleanStatus overrideEnabled = new BooleanStatus(false);
+    public static final FloatStatus overrideValue = new FloatStatus(0.0f);
 
     public static final EventOutput setTop = EventMixing.combine(raising.getSetTrueEvent(), lowering.getSetFalseEvent(), goingToMiddle.getSetFalseEvent());
     public static final EventOutput setMiddle = goingToMiddle.getSetTrueEvent();
@@ -38,7 +38,7 @@ public class Elevator {
 
     public static final BooleanStatus atTop = new BooleanStatus(false);
     public static final BooleanStatus atBottom = new BooleanStatus(true);
-    
+
     private static BooleanStatus lastLimitSide = new BooleanStatus(false); // true = top, false = bottom
 
     private static FloatInput winchSpeed = ControlInterface.mainTuning.getFloat("main-elevator-speed", 1.0f);
@@ -54,21 +54,21 @@ public class Elevator {
         Cluck.publish("CAN Elevator Any Fault", BooleanMixing.createDispatch(winchCAN.getDiagnosticChannel(ExtendedMotor.DiagnosticType.ANY_FAULT), updateCAN));
         Cluck.publish("CAN Elevator Bus Voltage Fault", BooleanMixing.createDispatch(winchCAN.getDiagnosticChannel(ExtendedMotor.DiagnosticType.BUS_VOLTAGE_FAULT), updateCAN));
         Cluck.publish("CAN Elevator Temperature Fault", BooleanMixing.createDispatch(winchCAN.getDiagnosticChannel(ExtendedMotor.DiagnosticType.TEMPERATURE_FAULT), updateCAN));
-        
+
         BooleanInput limitTop = BooleanMixing.createDispatch(BooleanMixing.invert(Igneous.makeDigitalInput(0)), Igneous.globalPeriodic);
         BooleanInput limitMiddleUpper = BooleanMixing.createDispatch(BooleanMixing.invert(Igneous.makeDigitalInput(2)), Igneous.globalPeriodic);
         BooleanInput limitMiddleLower = BooleanMixing.createDispatch(BooleanMixing.invert(Igneous.makeDigitalInput(3)), Igneous.globalPeriodic);
         BooleanInput limitBottom = BooleanMixing.createDispatch(BooleanMixing.invert(Igneous.makeDigitalInput(1)), Igneous.globalPeriodic);
-        
+
         atTop.setTrueWhen(EventMixing.filterEvent(raising, true, BooleanMixing.onPress(limitTop)));
         atBottom.setTrueWhen(EventMixing.filterEvent(lowering, true, BooleanMixing.onPress(limitBottom)));
-        
+
         atTop.setFalseWhen(EventMixing.filterEvent(lowering, true, BooleanMixing.onRelease(limitTop)));
         atBottom.setFalseWhen(EventMixing.filterEvent(raising, true, BooleanMixing.onRelease(limitBottom)));
-        
+
         atTop.setFalseWhen(BooleanMixing.onPress(BooleanMixing.orBooleans(limitMiddleUpper, limitMiddleLower, limitBottom)));
         atBottom.setFalseWhen(BooleanMixing.onPress(BooleanMixing.orBooleans(limitMiddleUpper, limitMiddleLower, limitTop)));
-        
+
         raising.setFalseWhen(EventMixing.filterEvent(atTop, true, Igneous.globalPeriodic));
         lowering.setFalseWhen(EventMixing.filterEvent(atBottom, true, Igneous.globalPeriodic));
 
@@ -76,7 +76,7 @@ public class Elevator {
         Cluck.publish("Elevator Top", setTop);
         Cluck.publish("Elevator Bottom", setBottom);
         Cluck.publish("Elevator Middle", setMiddle);
-        
+
         Cluck.publish("Elevator Raising", raising);
         Cluck.publish("Elevator Lowering", lowering);
         Cluck.publish("Elevator Aligning", goingToMiddle);
@@ -108,11 +108,6 @@ public class Elevator {
         module.setShouldBeRunning(goingToMiddle);
         module.updateWhen(QuasarHelios.globalControl);
 
-        /*
-        FloatMixing.pumpWhen(QuasarHelios.globalControl, Mixing.select(raising,
-                Mixing.select(lowering, FloatMixing.always(0.0f), FloatMixing.negate(winchSpeed)),
-                Mixing.select(lowering, winchSpeed, FloatMixing.always(0.0f))), winch);*/
-        
         FloatInputPoll main = Mixing.quadSelect(raising, lowering, FloatMixing.always(0.0f), FloatMixing.negate(winchSpeed), winchSpeed, FloatMixing.always(0.0f));
         FloatInputPoll override = new FloatInputPoll() {
             @Override
@@ -121,11 +116,11 @@ public class Elevator {
                 if (limitTop.get()) {
                     f = Math.min(0, f);
                 }
-                
+
                 if (limitBottom.get()) {
                     f = Math.max(0, f);
                 }
-                
+
                 return f;
             }
         };
