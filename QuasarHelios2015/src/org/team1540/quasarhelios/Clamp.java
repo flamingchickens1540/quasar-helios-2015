553e1a39a51ab5d921c60661e664b10078f67418
Alexander Mackworth <alexander@tssr.co>
2015-02-07 11:56:22 -0800

    Use actual encoder port for Clamp.

--- QuasarHelios2015/src/org/team1540/quasarhelios/Clamp.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Clamp.java
@@ -23,8 +23,7 @@ public class Clamp {
 
     public static void setup() {
 
-//        FloatInputPoll encoder = Igneous.makeEncoder(1, 2, false);
-        FloatInputPoll encoder = FloatMixing.always(0.0f);
+        FloatInputPoll encoder = Igneous.makeEncoder(10, 11, false);
         FloatOutput speedControl = Igneous.makeTalonMotor(11, Igneous.MOTOR_REVERSE, 0.1f);
 
         BooleanInput limitTop = BooleanMixing.createDispatch(Igneous.makeDigitalInput(2), Igneous.globalPeriodic);
