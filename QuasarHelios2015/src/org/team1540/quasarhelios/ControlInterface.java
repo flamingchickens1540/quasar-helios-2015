97d7b21895f3119e7fb3261cab8c54dae6fd5f65
Vincent Miller <millerv@catlin.edu>
2015-02-09 15:59:36 -0800

    Fixed issues with controls

--- QuasarHelios2015/src/org/team1540/quasarhelios/ControlInterface.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/ControlInterface.java
@@ -17,11 +17,76 @@ public class ControlInterface {
     public static TuningContext teleTuning = new TuningContext("Teleoperated").publishSavingEvent();
 
     public static void setup() {
-        setupJoysticks();
-        setupCluck();
+        setupDrive();
+        setupClamp();
+        setupElevator();
+        setupRollers();
     }
 
-    public static void setupJoysticks() {
+    private static void setupClamp() {
+        FloatMixing.pumpWhen(EventMixing.filterEvent(Igneous.joystick2.getButtonChannel(5), false, QuasarHelios.globalControl), Igneous.joystick2.getAxisChannel(2), Clamp.height);
+        Clamp.open.toggleWhen(Igneous.joystick2.getButtonSource(3));
+    }
+    
+    private static void setupRollers() {
+        EventInput povPressed = BooleanMixing.onPress(Igneous.joystick2.isPOVPressedSource(1));
+        FloatInput povAngle = Igneous.joystick2.getPOVAngleSource(1);
+        EventInput povUp = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, -0.1f, 0.1f), true, povPressed);
+        EventInput povDown = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, 179.9f, 180.1f), true, povPressed);
+        EventInput povLeft = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, 269.9f, 270.1f), true, povPressed);
+        // Uncomment this if you need it: 
+        // EventInput povRight = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, 89.9f, 90.1f), true, povPressed);
+
+        povUp.send(() -> {
+            if (Rollers.direction.get()) {
+                Rollers.running.set(true);
+            } else {
+                Rollers.running.set(!Rollers.running.get());
+            }
+
+            Rollers.direction.set(false);
+        });
+
+        povDown.send(() -> {
+            if (!Rollers.direction.get()) {
+                Rollers.running.set(true);
+            } else {
+                Rollers.running.set(!Rollers.running.get());
+            }
+
+            Rollers.direction.set(true);
+        });
+
+        Rollers.open.toggleWhen(povLeft);
+        
+        FloatInput cutoffRollers = mainTuning.getFloat("roller-override-threshold", 0.3f);
+        BooleanInputPoll overrideRollers = Igneous.joystick2.getButtonChannel(6);
+        
+        BooleanMixing.pumpWhen(QuasarHelios.globalControl, BooleanMixing.andBooleans(overrideRollers, FloatMixing.floatIsAtMost(
+                Igneous.joystick2.getAxisChannel(1), cutoffRollers)), Rollers.leftPneumaticOverride);
+        
+        BooleanMixing.pumpWhen(QuasarHelios.globalControl, BooleanMixing.andBooleans(overrideRollers, FloatMixing.floatIsAtLeast(
+                Igneous.joystick2.getAxisChannel(5), FloatMixing.negate(cutoffRollers))), Rollers.rightPneumaticOverride);
+
+        Igneous.joystick2.getAxisSource(2).send(Rollers.leftRollerOverride);
+        Igneous.joystick2.getAxisSource(6).send(Rollers.rightRollerOverride);
+        
+        FloatInput cutoffAuto = mainTuning.getFloat("autoloader-button-threshold", 0.5f);
+        
+        BooleanMixing.pumpWhen(QuasarHelios.globalControl, FloatMixing.floatIsAtLeast(Igneous.joystick2.getAxisSource(3), cutoffAuto), QuasarHelios.autoEjector);
+        BooleanMixing.pumpWhen(QuasarHelios.globalControl, FloatMixing.floatIsAtLeast(Igneous.joystick2.getAxisSource(4), cutoffAuto), QuasarHelios.autoLoader);
+    }
+    
+    private static void setupElevator() {
+        Igneous.joystick2.getButtonSource(4).send(Elevator.setTop);
+        Igneous.joystick2.getButtonSource(1).send(Elevator.setBottom);
+        Igneous.joystick2.getButtonSource(2).send(Elevator.stop);
+
+        BooleanMixing.pumpWhen(QuasarHelios.globalControl, Igneous.joystick2.getButtonChannel(6), Elevator.overrideEnabled);
+        Igneous.joystick2.getAxisSource(6).send(Elevator.overrideValue);
+    }
+    
+    private static void setupDrive() {
         FloatInput leftXAxis = Igneous.joystick1.getXAxisSource();
         FloatInput rightXAxis = Igneous.joystick1.getAxisSource(5);
         FloatInput leftYAxis = Igneous.joystick1.getYAxisSource();
@@ -38,69 +103,5 @@ public class ControlInterface {
         FloatMixing.deadzone(rightYAxis, .2f).send(DriveCode.rightJoystickY);
         DriveCode.octocanumShiftingButton = Igneous.joystick1.getButtonSource(1);
         DriveCode.recalibrateButton = Igneous.joystick1.getButtonSource(2);
-
-        Igneous.joystick2.getButtonSource(4).send(Elevator.setTop);
-        Igneous.joystick2.getButtonSource(1).send(Elevator.setBottom);
-        Igneous.joystick2.getButtonSource(2).send(Elevator.stop);
-        BooleanMixing.pumpWhen(QuasarHelios.globalControl, Igneous.joystick2.getButtonChannel(5), Elevator.overrideEnabled);
-
-        FloatInput axis2 = Igneous.joystick2.getAxisSource(2);
-        axis2.send(Elevator.overrideValue);
-        
-        EventInput povPressed = BooleanMixing.onPress(Igneous.joystick2.isPOVPressedSource(1));
-        FloatInput povAngle = Igneous.joystick2.getPOVAngleSource(1);
-        EventInput up = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, -0.1f, 0.1f), true, povPressed);
-        EventInput down = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, 179.9f, 180.1f), true, povPressed);
-        EventInput left = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, 269.9f, 270.1f), true, povPressed);
-        EventInput right = EventMixing.filterEvent(FloatMixing.floatIsInRange(povAngle, 89.9f, 90.1f), true, povPressed);
-        
-        up.send(() -> {
-            if (Rollers.direction.get()) {
-                Rollers.running.set(true);
-            } else {
-                Rollers.running.set(!Rollers.running.get());
-            }
-            
-            Rollers.direction.set(false);
-        });
-        
-        down.send(() -> {
-            if (!Rollers.direction.get()) {
-                Rollers.running.set(true);
-            } else {
-                Rollers.running.set(!Rollers.running.get());
-            }
-            
-            Rollers.direction.set(true);
-        });
-        
-        Rollers.open.toggleWhen(left);
-
-        FloatInput cutoffRollers = mainTuning.getFloat("roller-override-threshold", 0.3f);
-        BooleanInputPoll overrideRollers = Igneous.joystick2.getButtonChannel(6);
-
-        BooleanMixing.pumpWhen(QuasarHelios.globalControl, BooleanMixing.andBooleans(overrideRollers, FloatMixing.floatIsAtMost(
-                Igneous.joystick2.getAxisChannel(1), cutoffRollers)), Rollers.leftPneumaticOverride);
-
-        axis2.send(Rollers.leftRollerOverride);
-
-        BooleanMixing.pumpWhen(QuasarHelios.globalControl, BooleanMixing.andBooleans(overrideRollers, FloatMixing.floatIsAtLeast(
-                Igneous.joystick2.getAxisChannel(5), FloatMixing.negate(cutoffRollers))), Rollers.rightPneumaticOverride);
-
-        
-        FloatInput axis6 = Igneous.joystick2.getAxisSource(6);
-        axis6.send(Rollers.rightRollerOverride);
-
-        FloatMixing.pumpWhen(EventMixing.filterEvent(overrideRollers, false, QuasarHelios.globalControl), axis6, Clamp.height);
-        Clamp.open.toggleWhen(Igneous.joystick2.getButtonSource(3));
-
-        FloatInput cutoffAuto = mainTuning.getFloat("autoloader-button-threshold", 0.5f);
-
-        BooleanMixing.pumpWhen(QuasarHelios.globalControl, FloatMixing.floatIsAtLeast(Igneous.joystick2.getAxisSource(3), cutoffAuto), QuasarHelios.autoEjector);
-        BooleanMixing.pumpWhen(QuasarHelios.globalControl, FloatMixing.floatIsAtLeast(Igneous.joystick2.getAxisSource(4), cutoffAuto), QuasarHelios.autoLoader);
-    }
-
-    public static void setupCluck() {
-
     }
 }
