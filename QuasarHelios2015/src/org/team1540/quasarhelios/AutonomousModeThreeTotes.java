294958d60e6ed0f4b373ba9b6023b7fac273f5a2
Alexander Mackworth <alexander@tssr.co>
2015-02-13 17:26:38 -0800

    Fix three tote mode.

--- QuasarHelios2015/src/org/team1540/quasarhelios/AutonomousModeThreeTotes.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/AutonomousModeThreeTotes.java
@@ -1,5 +1,6 @@
 package org.team1540.quasarhelios;
 
+import ccre.channel.BooleanInputPoll;
 import ccre.channel.FloatInputPoll;
 import ccre.holders.TuningContext;
 import ccre.instinct.AutonomousModeOverException;
@@ -9,6 +10,8 @@ public class AutonomousModeThreeTotes extends AutonomousModeBase {
     public FloatInputPoll toteDistance;
     public FloatInputPoll autoZoneDistance;
     public FloatInputPoll strafeTime;
+    
+    public BooleanInputPoll containerToCollect; // true is first, false is second
 
     public AutonomousModeThreeTotes() {
         super("Three Tote Auto");
@@ -20,20 +23,24 @@ public class AutonomousModeThreeTotes extends AutonomousModeBase {
         // Collect first tote + container
         setClampHeight(1.0f);
         collectTote();
-        setClampHeight(0.0f);
-        setClampOpen(true);
-        drive(nudge.get());
-        setClampOpen(false);
-        setClampHeight(1.0f);
+        if (containerToCollect.get()) {
+            setClampHeight(0.0f);
+            setClampOpen(true);
+            drive(nudge.get());
+            setClampOpen(false);
+            setClampHeight(1.0f);
+        }
         // TODO: Hold container with top claw
         drive(toteDistance.get());
         // Collect next tote + container
         collectTote();
-        setClampHeight(0.0f);
-        setClampOpen(true);
-        drive(nudge.get());
-        setClampOpen(false);
-        setClampHeight(1.0f);
+        if (!containerToCollect.get()) {
+            setClampHeight(0.0f);
+            setClampOpen(true);
+            drive(nudge.get());
+            setClampOpen(false);
+            setClampHeight(1.0f);
+        }
         drive(toteDistance.get());
         // Collect last tote and drive to auto zone
         collectTote();
@@ -46,8 +53,6 @@ public class AutonomousModeThreeTotes extends AutonomousModeBase {
         DriveCode.octocanumShifting.set(true);
         strafe(1.0f, strafeTime.get());
         ejectTotes();
-        strafe(1.0f, strafeTime.get());
-        // TODO: Put down top container
         DriveCode.octocanumShifting.set(false);
     }
 
@@ -56,5 +61,6 @@ public class AutonomousModeThreeTotes extends AutonomousModeBase {
         this.toteDistance = context.getFloat("auto-three-totes-tote-distance", 7.0f);
         this.autoZoneDistance = context.getFloat("auto-three-totes-auto-zone-distance", 5.0f);
         this.strafeTime = context.getFloat("auto-three-totes-strafe-time", 1.0f);
+        this.containerToCollect = context.getBoolean("auto-three-totes-container-to-collect", true);
     }
 }
