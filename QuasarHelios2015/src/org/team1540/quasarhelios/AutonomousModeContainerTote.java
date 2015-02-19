bba6502d339958f0dfff1ba6985564b8ffda0045
AidanSmith <smithai@catlin.edu>
2015-02-19 15:26:17 -0800

    Oops, forgot this

---
+++ QuasarHelios2015/src/org/team1540/quasarhelios/AutonomousModeContainerTote.java
@@ -0,0 +1,62 @@
+package org.team1540.quasarhelios;
+
+import ccre.channel.FloatInputPoll;
+import ccre.holders.TuningContext;
+import ccre.instinct.AutonomousModeOverException;
+
+public class AutonomousModeContainerTote extends AutonomousModeBase {
+    protected FloatInputPoll toteDistance;
+    protected FloatInputPoll autoZoneDistance;
+    protected FloatInputPoll secondDistance;
+    
+    private FloatInputPoll containerTurnTime;
+    private FloatInputPoll containerDepositStrafeTime;
+    private FloatInputPoll autoZoneAngle;
+    private FloatInputPoll autoZoneSpeed;
+    private FloatInputPoll nudge;
+    private FloatInputPoll containerHeight;
+    private FloatInputPoll turnAngle;
+
+    public AutonomousModeContainerTote() {
+        super("Container, then Tote");
+    }
+
+    @Override
+    protected void runAutonomous() throws InterruptedException,
+            AutonomousModeOverException {
+        // Loading
+        pickupContainer(nudge.get());
+        setClampHeight(1.0f);
+        singleSideTurn((long) (containerTurnTime.get() * 1000), false);
+        collectTote();
+        // Motion
+        turn(autoZoneAngle.get(), true);
+        waitForTime(500);
+        drive(autoZoneDistance.get(), autoZoneSpeed.get());
+        waitForTime(500);
+        // Unload
+        ejectTotes();
+        //strafe(STRAFE_LEFT, containerDepositStrafeTime.get());
+        turn(-turnAngle.get(), true);
+        waitForTime(300);
+        drive(secondDistance.get(), autoZoneSpeed.get());
+        waitForTime(300);
+        depositContainer(containerHeight.get());
+        //drive(-returnDistance.get());
+        setClampHeight(1.0f);
+    }
+
+    public void loadSettings(TuningContext context) {
+        this.turnAngle = context.getFloat("Auto Mode Container Tote Turn Angle +A", -45.0f);
+        this.toteDistance = context.getFloat("Auto Mode Container Tote Tote Distance +A", 28.0f);
+        this.autoZoneDistance = context.getFloat("Auto Mode Container Tote Auto Zone Distance +A", 60.0f);
+        this.secondDistance = context.getFloat("Auto Mode Container Tote Second Distance +A", 24.0f);
+        this.nudge = context.getFloat("Auto Mode Container Tote Nudge +A", 12.0f);
+        this.containerTurnTime = context.getFloat("Auto Mode Container Tote Container Turn Time +A", 0.5f);
+        this.autoZoneAngle = context.getFloat("Auto Mode Container Tote Auto Zone Angle +A", 90.0f);
+        this.containerHeight = context.getFloat("Auto Mode Container Tote Container Height +A", 0.0f);
+        this.containerDepositStrafeTime = context.getFloat("Auto Mode Container Tote Container Deposit Strafe Time +A", 2.0f);
+        this.autoZoneSpeed = context.getFloat("Auto Mode Container Tote Auto Zone Speed", 1.0f);
+    }
+
+}
