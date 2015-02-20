4a32816abea1ee1bdb0a1681e6c937306099036a
Alexander Mackworth <alexander@tssr.co>
2015-02-19 17:00:16 -0800

    Optimization of container + tote mode.

--- QuasarHelios2015/src/org/team1540/quasarhelios/AutonomousModeContainerTote.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/AutonomousModeContainerTote.java
@@ -1,6 +1,7 @@
 package org.team1540.quasarhelios;
 
 import ccre.channel.FloatInputPoll;
+import ccre.ctrl.BooleanMixing;
 import ccre.holders.TuningContext;
 import ccre.instinct.AutonomousModeOverException;
 
@@ -24,26 +25,52 @@ public class AutonomousModeContainerTote extends AutonomousModeBase {
     @Override
     protected void runAutonomous() throws InterruptedException,
             AutonomousModeOverException {
-        // Loading
+        // Pickup container.
         pickupContainer(nudge.get());
         setClampHeight(1.0f);
         singleSideTurn((long) (containerTurnTime.get() * 1000), false);
-        collectTote();
+
+        // Load tote.
+        
+        // Move elevator.
+        boolean elevatorAtTop = Elevator.atTop.get();
+        if (!elevatorAtTop) {
+            Elevator.setTop.event();
+        }
+
+        // Move clamp.
+        float currentClampHeight = Clamp.heightReadout.get();
+        if (currentClampHeight < AutoLoader.clampHeightThreshold.get()) {
+            Clamp.mode.set(Clamp.MODE_HEIGHT);
+            Clamp.height.set(AutoLoader.clampHeightThreshold.get());
+            if (elevatorAtTop) {
+                waitUntil(Clamp.atDesiredHeight);
+            } else {
+                waitUntil(BooleanMixing.andBooleans(Clamp.atDesiredHeight, Elevator.atTop));
+            }
+        } else {
+            if (!elevatorAtTop) {
+                waitUntil(Elevator.atTop);
+            }
+        }
+        
+        // Run rollers.
+        Rollers.direction.set(Rollers.REVERSE);
+        Rollers.running.set(true);
+        Rollers.closed.set(true);
+
+        waitUntil(AutoLoader.crateInPosition);
+        
+        Rollers.running.set(false);
+        Rollers.closed.set(false);
+        
         // Motion
         turn(autoZoneAngle.get(), true);
         waitForTime(500);
         drive(autoZoneDistance.get(), autoZoneSpeed.get());
         waitForTime(500);
         // Unload
-        ejectTotes();
-        //strafe(STRAFE_LEFT, containerDepositStrafeTime.get());
-        turn(-turnAngle.get(), true);
-        waitForTime(300);
-        drive(secondDistance.get(), autoZoneSpeed.get());
-        waitForTime(300);
         depositContainer(containerHeight.get());
-        //drive(-returnDistance.get());
-        setClampHeight(1.0f);
     }
 
     public void loadSettings(TuningContext context) {
