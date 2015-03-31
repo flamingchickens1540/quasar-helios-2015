35dd0e04bb09fa6ea2e561b34f2998b3427a8da5
Colby Skeggs <skeggsc@catlin.edu>
2015-03-30 17:12:26 -0700

    Removed outdated autonomous modes.

--- QuasarHelios2015/src/org/team1540/quasarhelios/Autonomous.java
+++ QuasarHelios2015/src/org/team1540/quasarhelios/Autonomous.java
@@ -52,8 +52,6 @@ public class Autonomous {
         mainModule.addMode(new AutonomousModeNoodlePrep());
         mainModule.addMode(new AutonomousModeContainerTote());
         mainModule.addMode(new AutonomousModeThreeTotes());
-        mainModule.addMode(new AutonomousModeOneContainer());
-        mainModule.addMode(new AutonomousModeCalibration());
         mainModule.loadSettings(mainModule.addNullMode("none", "I'm a sitting chicken!"));
         Igneous.registerAutonomous(mainModule);
     }
